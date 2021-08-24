e2d6c0f97007715e05187660a2193b6f3c015569
jgoldsmidt <julieta.gold@gmail.com>
2021-08-23 22:11:14 -0300

    d

--- config/db.js
+++ config/db.js
@@ -1,18 +1,18 @@
 //Conexion MySQL
 const Sequelize = require('sequelize')
 
-//conexion local -> wamp
-let database = 'utn2021'
-let userMYSQL = 'root'
-let passMySQL = ''
-let hostMySQL = '127.0.0.1'
+//conexion a BBDD
+let database = 'heroku_9fa156ca2c902ed'
+let userMYSQL = 'bc419ed49598b9'
+let passMySQL = 'e9c23c9c'
+let hostMySQL = 'us-cdbr-east-04.cleardb.com'
 
 
 
 const db = new Sequelize(database, userMYSQL, passMySQL, {
-  host: hostMySQL,
-  dialect: 'mysql',
-  define: {
+    host: hostMySQL,
+    dialect: 'mysql',
+    define: {
         //desactivamos el timestamp, para que no envie fechas de actualizaciones a la db
         //https://sequelize.org/v5/manual/models-definition.html
         //https://sequelize.org/v5/manual/models-definition.html#configuration
