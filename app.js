e7f107ccf5a8a77e5adad968ed77a5123d8c2876
jgoldsmidt <julieta.gold@gmail.com>
2021-08-23 22:03:08 -0300

    subida base de app

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs = require('hbs')
 
+//Listado de Rutas
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var contactoRouter = require('./routes/contacto');
+var productosRouter = require('./routes/productos');
+var altaproductosRouter = require('./routes/altaProducto');
+//formato de importacion de ES10 -> import {productoRouter} from './routes/productos.js'
 
 var app = express();
+
+//registramos los Partials
+hbs.registerPartials(__dirname + '/views/partials')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +28,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//ejecutar la ruta
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/usuarios', usersRouter);
+app.use('/contacto', contactoRouter);
+app.use('/productos', productosRouter);
+app.use('/nuevo-producto', altaproductosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/contacto.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const router = express.Router()
+//importamos Nodemailer
+const nodemailer = require("nodemailer");
+
+
+let title = "Contacto"
+
+let year = new Date().getFullYear();
+
+router.get('/',function(request,response){
+    // response.send("Esto es el contacto")
+    //render('vista a renderizar',{data a enviar a la vista})
+    response.render('contacto.hbs',{data,year})
+})
+
+router.post('/', (req,res) => {
+    // console.log(req.body.nombre)
+    // console.log(req.body.email)
+
+    const user = {
+        nombre: req.body.nombre,
+        email: req.body.email,
+        mensaje: req.body.mensaje
+    } 
+
+    //Enviar datos por email -> Nodemailer
+    async function main(){
+
+        //transporter
+        const transporter = require('../config/transporter')
+
+        // Envio, segun la conexion del Transport
+        let info = await transporter.sendMail({
+            from: `${user.nombre} <${user.email}>`, // sender address
+            to: "guido.varela@gmail.com", // list of receivers
+            subject: "Nuevo Mensaje", // Subject line
+            //text: "Hello world?", // plain text body
+            html: `<strong>Nuevo Contacto</strong>
+            <p>Nombre: ${user.nombre}</p>
+            <p>Email: ${user.email}</p>
+            <p>Mensaje: ${user.mensaje}</p>`, // html body
+        });
+
+        //console.log("Message sent: %s", info.messageId);
+        console.log(info)
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+        res.render('enviocontacto',{user, title, year, envio:true})
+    }
+
+    //main().catch(res.render("errorcontacto.hbs",{error:"error"}));
+    main().catch(console.error)
+        
+})
+
+module.exports = router;
