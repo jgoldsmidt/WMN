6970253eaa45c8b7138f638faebfceaa14ba912b
jgoldsmidt <julieta.gold@gmail.com>
2021-08-24 03:27:12 -0300

    i

--- app.js
+++ app.js
@@ -10,30 +10,30 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var contactoRouter = require('./routes/contacto');
 var librosRouter = require('./routes/libros');
-var altalibrosRouter = require('./routes/altaLibro');
+var altaLibrosRouter = require('./routes/altaLibro');
 //formato de importacion de ES10 -> import {productoRouter} from './routes/productos.js'
 
 var app = express();
 
 //registramos los Partials
-hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerPartials(__dirname + '/views/partials');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.use(logger('dev'));
+/*app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));*/
 
 //ejecutar la ruta
 app.use('/', indexRouter);
 app.use('/usuarios', usersRouter);
 app.use('/contacto', contactoRouter);
 app.use('libros', librosRouter);
-app.use('/nuevo-libro', altalibrosRouter);
+app.use('/nuevo-libro', altaLibrosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
